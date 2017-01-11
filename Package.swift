a5f69db4fd9deb7f855bd642054917a5e29676cf
Swizzlr <git@swizzlr.co>
2017-01-11 23:05:38 +0000

    Update script and templates.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,13 @@
 import PackageDescription
 
-let package = Package(
+var package = Package(
     name: "TitanRouter",
     dependencies: [
       .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0)
     ]
+)
+
+// Uncomment the following to install sourcery for development
+package.dependencies.append(
+ .Package(url: "https://github.com/krzysztofzablocki/Sourcery.git", "0.5.0")
 )
