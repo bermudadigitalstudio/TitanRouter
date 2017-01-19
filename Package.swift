fcb76dcd8f8b340b8cec628a1911298d78fbbbb5
Swizzlr <git@swizzlr.co>
2017-01-19 10:27:08 +0000

    Revert "Merge pull request #5 from bermudadigitalstudio/swizzlr/extraction"
    This reverts commit 659d4826c7258ea6da3afe99d8018a5a799c3c0a, reversing
    changes made to b77b9f5898094bb1886d8ef24f32f081fe8194ce.

--- Package.swift
+++ Package.swift
@@ -6,3 +6,8 @@ var package = Package(
       .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 2)
     ]
 )
+
+// Uncomment the following to install sourcery for development
+//package.dependencies.append(
+//  .Package(url: "https://github.com/krzysztofzablocki/Sourcery.git", "0.5.3")
+//)
--- Tests/TitanRouterTests/TitanRouterTests.swift
+++ Tests/TitanRouterTests/TitanRouterTests.swift
@@ -2,88 +2,142 @@ import XCTest
 import TitanRouter
 import TitanCore
 
-extension String: ResponseType {
-  public var body: String {
-    return self
-  }
-
-  public var code: Int {
-    return 200
-  }
-
-  public var headers: [Header] {
-    return []
-  }
-}
-
-extension Int: ResponseType {
-  public var code: Int { return self }
-  public var body: String { return "" }
-  public var headers: [Header] { return [] }
-}
-
 final class TitanRouterTests: XCTestCase {
   var app: Titan!
   override func setUp() {
     app = Titan()
   }
+  func testMutableParams() {
+    let app = Titan()
+    app.get("/init") { (req, res) -> Void in
+      res.body = "Hello World"
+      req.path = "/rewritten"
+      res.code = 500
+    }
+    app.addFunction { (req, res) -> (RequestType, ResponseType) in
+      XCTAssertEqual(req.path, "/rewritten")
+      return (req, res)
+    }
+    let response = app.app(request: Request("GET", "/init"))
+    XCTAssertEqual(response.code, 500)
+    XCTAssertEqual(response.body, "Hello World")
+  }
+
+  func testFunctionalMutableParams() {
+    let app = Titan()
+    app.get("/init") { (req: inout Request, res: inout Response) -> (RequestType, ResponseType) in
+      var newReq = req
+      var newRes = res
+      newRes.body = "Hello World"
+      newReq.path = "/rewritten"
+      newRes.code = 500
+      // Check that mutating the inout params has no effect on the function chain – ONLY the returned values should matter
+      res.code = 400
+      res.body = "Should not ever come into the response"
+      return (newReq, newRes)
+    }
+    app.addFunction { (req, res) -> (RequestType, ResponseType) in
+      XCTAssertEqual(req.path, "/rewritten")
+      return (req, res)
+    }
+    let response = app.app(request: Request("GET", "/init"))
+    XCTAssertEqual(response.code, 500)
+    XCTAssertEqual(response.body, "Hello World")
+  }
 
   func testBasicGet() {
-    app.route(method: "GET", path: "/username") { req, res in
-      return (req, "swizzlr")
+    app.get("/username") {
+      return "swizzlr"
     }
     XCTAssertEqual(app.app(request: Request("GET", "/username")).body, "swizzlr")
   }
 
   func testTitanEcho() {
-    app.route(path: "/echoMyBody") { req, _ in
-      return (req, req.body)
+    app.get("/echoMyBody") { req in
+      return req.body
     }
     XCTAssertEqual(app.app(request: Request("GET", "/echoMyBody", "hello, this is my body")).body,
                    "hello, this is my body")
   }
 
   func testMultipleRoutes() {
-    app.route(path: "/username") { req, _ in
-      return (req, "swizzlr")
+    app.get("/username") {
+      return "swizzlr"
     }
 
-    app.route(path: "/echoMyBody") { req, _ in
-      return (req, req.body)
+    app.get("/echoMyBody") { req in
+      return req.body
     }
     XCTAssertEqual(app.app(request: Request("GET", "/echoMyBody", "hello, this is my body")).body,
                    "hello, this is my body")
     XCTAssertEqual(app.app(request: Request("GET", "/username")).body, "swizzlr")
   }
 
+  func testTitanSugar() {
+    let somePremadeFunction: Function = { req, res in
+      return (req, res)
+    }
+    app.get(path: "/username", handler: somePremadeFunction)
+    app.get("/username", somePremadeFunction)
+  }
+
   func testMiddlewareFunction() {
     var start = Date()
     var end = start
-    app.addFunction(path: "*") {
+    app.addFunction("*") {
       start = Date()
-      return $0
     }
-    app.route(path: "/username") { req, _ in
-      return (req, "swizzlr")
+    app.get("/username") {
+      return "swizzlr"
     }
-    app.addFunction(path: "*") {
+    app.addFunction("*") {
       end = Date()
-      return $0
     }
     _ = app.app(request: Request("GET", "/username"))
     XCTAssertNotEqual(start, end)
   }
 
+  func testDifferentMethods() {
+    app.get("/getSomething") {
+      return "swizzlrGotSomething!"
+    }
+
+    app.post("/postSomething") {
+      return "something posted"
+    }
+
+    app.put("/putSomething") {
+      return "i can confirm that stupid stuff is now on the server"
+    }
+
+    app.patch("/patchSomething") {
+      return "i guess we don't have a flat tire anymore?"
+    }
+
+    app.delete("/deleteSomething") {
+      return "error: could not find the USA or its principles"
+    }
+
+    app.options("/optionSomething") {
+      return "I sold movie rights!"
+    }
+
+    app.head("/headSomething") {
+      return "OWN GOAL!!"
+    }
+
+  }
+
   func testSamePathDifferentiationByMethod() {
     var username = ""
 
-    app.route(method: "get", path: "/username") { req, _ in
-      return (req, username)
+    app.get("/username") {
+      return username
     }
 
-    app.route(method: "post", path: "/username") { req, _ in
+    app.post("/username") { (req: RequestType) -> Int in
       username = req.body
-      return (req, 201)
+      return 201
     }
 
     let resp = app.app(request: Request("POST", "/username", "Lisa"))
